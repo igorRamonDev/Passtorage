2bc8c2a82ac51f1c07954a350f5c2cd0bfa959e0
Igor Oliveira <igorramoncastro@gmail.com>
2024-08-14 22:26:55 -0300

    Passtorage

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-function generatePassword() {
-    const length = parseInt(document.getElementById('length').value);
-    const includeUppercase = document.getElementById('uppercase').checked;
-    const includeSpecial = document.getElementById('special').checked;
-    const includeNumbers = document.getElementById('numbers').checked;
+function generatePassword(length, charset, word) {
+    let password = "";
 
-    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
-    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const specialChars = '!@#$%^&*()_+[]{}|;:,.<>?';
-    const numberChars = '0123456789';
-
-    let charset = lowercaseChars;
-    if (includeUppercase) charset += uppercaseChars;
-    if (includeSpecial) charset += specialChars;
-    if (includeNumbers) charset += numberChars;
-
-    let password = '';
+    if (word) {
+        password += word;
+        length -= word.length; // Reduz o comprimento restante para a senha
+    }
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * charset.length);
         password += charset[randomIndex];
     }
 
-    document.getElementById('generatedPassword').textContent = password;
+    document.getElementById("generatedPassword").textContent = password;
+}
+
+function defaultPassword() {
+    const length = parseInt(document.getElementById("length").value) || 12;
+    const word = document.getElementById("word").value;
+    const includeUppercase = document.getElementById("uppercase").checked;
+    const includeSpecial = document.getElementById("special").checked;
+    const includeNumbers = document.getElementById("numbers").checked;
+
+    if (includeUppercase || includeSpecial || includeNumbers || word) {
+        const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+        const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const specialChars = "!@#$%^&*()_+[]{}|;:,.<>?";
+        const numberChars = "0123456789";
+
+        let charset = lowercaseChars;
+        if (includeUppercase) charset += uppercaseChars;
+        if (includeSpecial) charset += specialChars;
+        if (includeNumbers) charset += numberChars;
+
+        generatePassword(length, charset, word);
+    } else {
+        const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+        generatePassword(length, lowercaseChars, word);
+    }
 }
